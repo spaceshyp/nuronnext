51f7d6a4d764bf74fa2e129a881914043569216c
spaceshyp <engineering@spaceshyp.com>
2022-04-25 14:05:55 -0700

    first commit

---
+++ src/pages/minting.jsx
@@ -0,0 +1,56 @@
+import PropTypes from "prop-types";
+import SEO from "@components/seo";
+import Wrapper from "@layout/wrapper";
+import Header from "@layout/header/header-01";
+import Footer from "@layout/footer/footer-01";
+import AboutArea from "@containers/about/layout-02";
+import QuoteArea from "@containers/quote-area";
+import FunfactArea from "@containers/funfact";
+import CTAArea from "@containers/cta";
+import BlogArea from "@containers/blog/layout-01";
+import { normalizedData } from "@utils/methods";
+import { getAllPosts } from "../lib/api";
+
+// Demo data
+import aboutData from "../data/innerpages/about.json";
+
+const About = ({ posts }) => {
+    const content = normalizedData(aboutData?.content || []);
+    return (
+        <Wrapper>
+            <SEO pageTitle="About" />
+            <Header />
+            <main id="main-content">
+                <h1 style={{ display: "flex", justifyContent: "center" }}>
+                    {" "}
+                    Coming Soon{" "}
+                </h1>
+            </main>
+            <Footer />
+        </Wrapper>
+    );
+};
+
+export async function getStaticProps() {
+    const posts = getAllPosts([
+        "title",
+        "date",
+        "slug",
+        "image",
+        "category",
+        "timeToRead",
+    ]);
+
+    return {
+        props: {
+            posts: posts.slice(0, 4),
+            className: "template-color-1",
+        },
+    };
+}
+
+About.propTypes = {
+    posts: PropTypes.arrayOf(PropTypes.shape({})),
+};
+
+export default About;
